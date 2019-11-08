1f7865083ced2c61bcbcbbfc61d5576c2acf0a38
kh45 <khaledhassan45@gmail.com>
2019-11-08 16:39:22 -0500

    render decks on side and click on deck for all cards

--- src/script.js
+++ src/script.js
@@ -1,15 +1,49 @@
+const header = document.getElementById('greeting')
+const decks = document.getElementById('decks')
+
 const main = document.getElementById('main')
 document.addEventListener('DOMContentLoaded', () => {
-    fetch("http://localhost:3000/users")
+    fetch("http://localhost:3000/users/17")
     .then(response => response.json())
-    .then(cards => cards.forEach(card => renderCard(card)))
+    //.then(cards => cards.forEach(card => renderCard(card)))
+    .then(card => userShowPage(card))
+
 })
 
+function userShowPage(user) {
+    header.innerText = `Welcome! ${user.name}`
+    const decksList = document.createElement('ul')
+    user.decks.forEach(deck => {
+        const user_deck = document.createElement('li');
+        user_deck.innerText = deck.name;
+        user_deck.id = `deck-${deck.id}`
+        decksList.appendChild(user_deck)
+        decks.appendChild(decksList)
+        user_deck.addEventListener('click', () => {
+            fetchDeck()
+            .then(response => response.json())
+            .then(deck => deck.cards.forEach(renderCards))
+            //.then(cards => console.log(cards))
+        })
+    })
 
-function renderCard (card) {
+
+
+
+
+}
+
+
+function renderCards (card) {
+    main.appendChild(header)
     const carDiv = document.createElement('div');
     const image = document.createElement('img');
     image.src = card.imageUrl
     carDiv.appendChild(image);
     main.appendChild(carDiv)
+}
+
+function fetchDeck() {
+    const deck_id = event.target.id.split("-")[1]
+    return fetch(`http://localhost:3000/decks/${deck_id}`)
 }
