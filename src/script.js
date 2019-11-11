12741e444243f6d5733d1947d4e3d7b83f592a3f
kh45 <khaledhassan45@gmail.com>
2019-11-11 09:16:59 -0500

    sidebar working

--- src/script.js
+++ src/script.js
@@ -1,13 +1,22 @@
 const header = document.getElementById('greeting')
 const decks = document.getElementById('decks')
-const deck_cards = document.getElementById('cards')
+const deckCards = document.getElementById('cards')
+const currentDeck = document.getElementById('current-deck')
+const gyms = Array.from(document.getElementsByClassName('gym'))
+const backCardImg = "https://images.pokemontcg.io/xyp/XY154_hires.png"
+const body = document.getElementsByTagName('body')[0]
 
 const main = document.getElementById('main')
 document.addEventListener('DOMContentLoaded', () => {
-    fetch("http://localhost:3000/users/17")
+    fetch("http://localhost:3000/users")
     .then(response => response.json())
-    //.then(cards => cards.forEach(card => renderCard(card)))
-    .then(card => userShowPage(card))
+    .then(user => userShowPage(user[0]))
+
+    gyms.forEach(gym => {
+    gym.addEventListener('click', () => {
+        renderGymBoard()
+    })
+})
 
 })
 
@@ -21,30 +30,31 @@ function userShowPage(user) {
         decksList.appendChild(user_deck)
         decks.appendChild(decksList)
         user_deck.addEventListener('click', () => {
+            currentDeck.innerText = `Currently Displaying: ${event.target.innerText}`
+            deckCards.innerText = ""
             fetchDeck()
             .then(response => response.json())
             .then(deck => deck.cards.forEach(renderCards))
-            //.then(cards => console.log(cards))
         })
     })
-
-
-
-
-
 }
 
-
 function renderCards (card) {
-   // main.appendChild(header)
-    // const carDiv = document.createElement('div');
+    const cardDiv = document.createElement('div');
     const image = document.createElement('img');
+    image.classList.add('single-card');
     image.src = card.imageUrl
-    // carDiv.appendChild(image);
-    deck_cards.appendChild(image)
+    cardDiv.appendChild(image);
+    deckCards.appendChild(cardDiv)
 }
 
 function fetchDeck() {
     const deck_id = event.target.id.split("-")[1]
     return fetch(`http://localhost:3000/decks/${deck_id}`)
+}
+
+function renderGymBoard () {
+    body.innerText = ''
+    //create user hand/deck/active card
+    //do the same for computer
 }
